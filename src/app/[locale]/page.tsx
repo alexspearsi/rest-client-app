9c98e3cb4fb0c433eedc8a1afe9f47191ec79595
Olya Dubodel <dubodel.olya@gmail.com>
2025-09-05 14:38:34 +0300

    refactor: replace h1 with custom Heading component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,14 @@
 import { useTranslations } from 'next-intl';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import { Heading } from '@/components/ui/typography';
 
 export default function Page() {
   const t = useTranslations('HomePage');
 
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-6 p-8">
-      <h1 className="text-3xl font-bold">{t('title')}</h1>
+      <Heading size="h1">{t('title')}</Heading>
       <div className="flex gap-4">
         <Button variant="default" asChild>
           <Link href={`/authentication`}>{t('signin')}</Link>
