31192172d192b0c523fea38842e043608d50455f
Michael <fatlycka@gmail.com>
2025-09-07 23:27:32 +0300

    chore: test deploy

--- src/stores/main-store.ts
+++ src/stores/main-store.ts
@@ -11,3 +11,5 @@ export const useMainStore = create<MainStore>((set) => ({
   toggle: () => set((state) => ({ someState: !state.someState })),
   setState: (value) => set({ someState: value }),
 }));
+
+// ПРОВЕРКА ДЕПЛОЯ РАЗ ДВА ТРИ
