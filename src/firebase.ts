e099fe183f1c2d40ff996398e347f9caf36c44bb
Alex <strelchenko.aleksandr@yandex.ru>
2025-09-10 21:26:02 +0300

    chore: move Firebase config to .env

--- src/firebase.ts
+++ src/firebase.ts
@@ -11,13 +11,13 @@ import {
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDn3OfLMU-ZDHDP1c8Z3KCOblbC7I6QEWQ',
-  authDomain: 'rest-client-app-acfcf.firebaseapp.com',
-  projectId: 'rest-client-app-acfcf',
-  storageBucket: 'rest-client-app-acfcf.firebasestorage.app',
-  messagingSenderId: '750790602672',
-  appId: '1:750790602672:web:9f0fd6c4bf8a1624a3f075',
-  measurementId: 'G-ZKEHT9M2SK',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
 
 const app = initializeApp(firebaseConfig);
