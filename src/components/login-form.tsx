141905372df70b8cf0362147e414f2c28ef08ce8
Olya Dubodel <dubodel.olya@gmail.com>
2025-09-15 19:52:23 +0300

    feat: improve auth forms with react-hook-form, zod validation and extract them into separate components

---
+++ src/components/login-form.tsx
@@ -0,0 +1,112 @@
+'use client';
+
+import { useCallback, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useTranslations } from 'next-intl';
+import { toast } from 'sonner';
+import { Lock, Mail } from 'lucide-react';
+import { LoginSchema, useLoginSchema } from '@/lib/auth-schemas';
+import { logInWithEmailAndPassword } from '@/firebase';
+import { Button } from './ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
+import { Input } from './ui/input';
+import ShowHideButton from './ui/show-hide-button';
+
+export default function LoginForm() {
+  const t = useTranslations('LoginForm');
+  const tt = useTranslations('Notification');
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const loginSchema = useLoginSchema();
+
+  const form = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const handleLogIn = useCallback(
+    async ({ email, password }: LoginSchema) => {
+      const id = toast.loading(tt('loggingIn'));
+
+      try {
+        await logInWithEmailAndPassword(email, password);
+        toast.success(tt('loginSuccess'), { id });
+      } catch {
+        toast.error(tt('loginFailed'), { id });
+      }
+    },
+    [tt],
+  );
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleLogIn)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem className="relative pb-6">
+              <FormLabel>{t('emailLabel')} *</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder={t('emailPlaceholder')}
+                  type="text"
+                  contentBefore={
+                    <Mail className="text-muted-foreground h-4 w-4" />
+                  }
+                  className="h-12"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="absolute bottom-0 left-0" />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem className="relative pb-6">
+              <FormLabel>{t('passwordLabel')} *</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder={t('passwordPlaceholder')}
+                  type={showPassword ? 'text' : 'password'}
+                  contentBefore={
+                    <Lock className="text-muted-foreground h-4 w-4" />
+                  }
+                  contentAfter={
+                    <ShowHideButton
+                      onClick={() => setShowPassword(!showPassword)}
+                      show={showPassword}
+                      className="text-muted-foreground h-4 w-4"
+                    />
+                  }
+                  className="h-12"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="absolute bottom-0 left-0" />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="h-12 w-full">
+          {t('login')}
+        </Button>
+      </form>
+    </Form>
+  );
+}
---
+++ src/components/register-form.tsx
@@ -0,0 +1,139 @@
+'use client';
+
+import { useCallback, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useRouter } from '@/i18n/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useTranslations } from 'next-intl';
+import { toast } from 'sonner';
+import { Lock, Mail, User } from 'lucide-react';
+import { useRegisterSchema, RegisterSchema } from '@/lib/auth-schemas';
+import { registerWithEmailAndPassword } from '@/firebase';
+import { Button } from './ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
+import { Input } from './ui/input';
+import ShowHideButton from './ui/show-hide-button';
+
+export default function RegisterForm() {
+  const t = useTranslations('SignupForm');
+  const tt = useTranslations('Notification');
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const router = useRouter();
+
+  const registerSchema = useRegisterSchema();
+
+  const form = useForm<RegisterSchema>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const handleSignUp = useCallback(
+    async ({ name, email, password }: RegisterSchema) => {
+      const id = toast.loading(tt('creatingAccount'));
+
+      try {
+        await registerWithEmailAndPassword(name, email, password);
+        toast.success(tt('signupSuccess'), { id });
+        router.replace('/');
+      } catch {
+        toast.error(tt('signupFailed'), { id });
+      }
+    },
+    [tt, router],
+  );
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSignUp)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem className="relative pb-6">
+              <FormLabel>{t('nameLabel')} *</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder={t('namePlaceholder')}
+                  type="text"
+                  contentBefore={
+                    <User className="text-muted-foreground h-4 w-4" />
+                  }
+                  className="h-12"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="absolute bottom-0 left-0" />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem className="relative pb-6">
+              <FormLabel>{t('emailLabel')} *</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder={t('emailPlaceholder')}
+                  type="text"
+                  contentBefore={
+                    <Mail className="text-muted-foreground h-4 w-4" />
+                  }
+                  className="h-12"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="absolute bottom-0 left-0" />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem className="relative pb-6">
+              <FormLabel>{t('passwordLabel')} *</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder={t('passwordPlaceholder')}
+                  type={showPassword ? 'text' : 'password'}
+                  contentBefore={
+                    <Lock className="text-muted-foreground h-4 w-4" />
+                  }
+                  contentAfter={
+                    <ShowHideButton
+                      onClick={() => setShowPassword(!showPassword)}
+                      show={showPassword}
+                      className="text-muted-foreground h-4 w-4"
+                    />
+                  }
+                  className="h-12"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="absolute bottom-0 left-0" />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="h-12 w-full">
+          {t('signup')}
+        </Button>
+      </form>
+    </Form>
+  );
+}
