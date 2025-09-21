97e9da5f5fcc1ce3ea3cd73bc29c1193c2beaee5
Olya Dubodel <dubodel.olya@gmail.com>
2025-09-21 19:58:17 +0300

    feat: add translation to the not found page

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,21 +1,23 @@
 import { Button } from '@/components/ui/button';
 import { Heading } from '@/components/ui/typography';
 import { Link } from '@/i18n/navigation';
+import { useTranslations } from 'next-intl';
 
 export default function NotFound() {
+  const t = useTranslations('NotFoundPage');
+
   return (
     <section className="flex min-h-screen items-center justify-center">
       <div className="container mx-auto space-y-8 px-6 text-center lg:px-20">
         <div className="space-y-6">
           <span className="text-primary block text-5xl">404</span>
-          <Heading size="h2">Page Not Found</Heading>
+          <Heading size="h2">{t('title')}</Heading>
           <p className="text-muted-foreground mx-auto max-w-2xl text-lg">
-            Sorry, we couldn&apos;t find the page you&apos;re looking for.
-            Please check the URL or navigate back home.
+            {t('description')}
           </p>
         </div>
         <Button variant="default">
-          <Link href="/">Go back home</Link>
+          <Link href="/">{t('goBackHome')}</Link>
         </Button>
       </div>
     </section>
