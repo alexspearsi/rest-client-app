319049cabe62b2b6c579646a6833bc62c9939fe8
Olya Dubodel <dubodel.olya@gmail.com>
2025-10-16 18:21:11 +0300

    style: improve styles for the variable page

--- src/components/variables/variables.tsx
+++ src/components/variables/variables.tsx
@@ -42,7 +42,7 @@ export default function Variables(): JSX.Element {
   }
 
   return (
-    <div className="relative space-y-4">
+    <div className="relative space-y-3">
       <div className="bg-secondary sticky top-0 left-0 z-10 flex items-center justify-between border-b p-4">
         <Label className="text-lg font-semibold">{t('subtitle')}</Label>
         <CustomTooltip content={t('addTooltip')}>
